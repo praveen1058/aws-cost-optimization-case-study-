1d73d29cf31337787eb3419316cb450920367e5c
praveen1058 <74782423+praveen1058@users.noreply.github.com>
2026-08-10 19:05:13 +0530

    Update variables.tf

--- terraform/budgets/variables.tf
+++ terraform/budgets/variables.tf
@@ -1,1 +1,28 @@
+variable "aws_region" {
+description = "AWS region where the supporting resources are deployed."
+type        = string
+default     = "ap-south-1"
+}
+
+variable "budget_name" {
+description = "Name of the AWS monthly cost budget."
+type        = string
+default     = "aws-cost-optimization-monthly-budget"
+}
+
+variable "monthly_budget" {
+description = "Maximum monthly AWS budget in USD."
+type        = number
+default     = 100
+}
+
+variable "notification_email" {
+description = "Email address that receives AWS Budget notifications."
+type        = string
+
+validation {
+condition     = can(regex("^[^@\s]+@[^@\s]+\.[^@\s]+$", var.notification_email))
+error_message = "notification_email must be a valid email address."
+}
+}
 
