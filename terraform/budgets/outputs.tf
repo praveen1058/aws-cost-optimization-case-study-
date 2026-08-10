117573ce776769e9f774e4517b51dec8590d58f4
praveen1058 <74782423+praveen1058@users.noreply.github.com>
2026-08-10 19:05:48 +0530

    Update outputs.tf

--- terraform/budgets/outputs.tf
+++ terraform/budgets/outputs.tf
@@ -1,1 +1,15 @@
 
+output "budget_name" {
+description = "Name of the AWS budget."
+value       = aws_budgets_budget.monthly_cost.name
+}
+
+output "monthly_budget" {
+description = "Configured monthly AWS budget."
+value       = aws_budgets_budget.monthly_cost.limit_amount
+}
+
+output "budget_arn" {
+description = "ARN of the AWS budget."
+value       = aws_budgets_budget.monthly_cost.arn
+}
